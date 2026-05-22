e96d49263794b34d133583ac63f4074df513c440
Amit Fishel <amit.fishel@post.runi.ac.il>
2026-05-22 12:10:25 +0300

    Implement Board class basics

--- EX02/Logic/Board.cs
+++ EX02/Logic/Board.cs
@@ -6,66 +6,87 @@ using System.Threading.Tasks;
 
 namespace EX02.Logic
 {
+    internal enum eCellValue
+    {
+        Empty,
+        X,
+        O
+    }
     internal class Board
     {
-        private char[,] m_Cells;
+        private eCellValue[,] m_Cells;
         private int m_Size;
 
-        /// <summary>
+        
         /// Creates a square board with the given size.
-        /// </summary>
         public Board(int i_Size)
         {
+            m_Size = i_Size;
+
+            m_Cells = new eCellValue[m_Size, m_Size];
+
+            Clear();
+
+
         }
 
-        /// <summary>
         /// Returns the board size.
-        /// </summary>
         public int Size
         {
             get
             {
+                return m_Size;
             }
         }
 
-        /// <summary>
+        
         /// Checks if a specific cell is empty.
-        /// </summary>
         public bool IsCellEmpty(int i_Row, int i_Col)
         {
+            return m_Cells[i_Row, i_Col] == eCellValue.Empty;
         }
 
-        /// <summary>
+
         /// Places a symbol inside the requested cell.
-        /// </summary>
-        public void PlaceSymbol(int i_Row, int i_Col, char i_Symbol)
+        public void PlaceSymbol(int i_Row, int i_Col, eCellValue i_Symbol)
+
         {
+            m_Cells[i_Row, i_Col] = i_Symbol;
         }
 
-        /// <summary>
+
         /// Returns the symbol stored in a cell.
-        /// </summary>
-        public char GetCell(int i_Row, int i_Col)
+        public eCellValue GetCell(int i_Row, int i_Col)
         {
+            return m_Cells[i_Row, i_Col];
         }
 
-        /// <summary>
+
         /// Checks whether the board is completely full.
-        /// </summary>
         public bool IsFull()
         {
+            for (int row = 0; row < m_Size; row++)
+            {
+                for (int col = 0; col < m_Size; col++)
+                {
+                    if (m_Cells[row, col] == eCellValue.Empty)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
         }
 
-        /// <summary>
+     
         /// Checks if the given symbol created a losing sequence.
-        /// </summary>
         public bool HasLosingSequence(char i_Symbol)
         {
+
         }
 
-        /// <summary>
+       
         /// Clears all cells for a new round.
-        /// </summary>
         public void Clear()
         {
         }
